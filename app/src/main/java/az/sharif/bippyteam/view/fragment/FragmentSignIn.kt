6f62a90a61bab51d2bdccb2668b1c9eb7f78a81d
Jafar Tarverdiyev <cefertarverdiyevv@gmail.com>
2023-03-29 14:58:01 +0400

    login

--- app/src/main/java/az/sharif/bippyteam/view/fragment/FragmentSignIn.kt
+++ app/src/main/java/az/sharif/bippyteam/view/fragment/FragmentSignIn.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import az.sharif.bippyteam.R
 import az.sharif.bippyteam.databinding.FragmentSignInBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FragmentSignIn:Fragment() {
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var binding:FragmentSignInBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        firebaseAuth= Firebase.auth
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +30,27 @@ class FragmentSignIn:Fragment() {
 
         binding.createAccount.setOnClickListener{
             findNavController().navigate(R.id.action_signInFragment_to_signUpFragment)
+        }
+        var email = binding.inputEmail
+        var password  = binding.inputPassword
+
+        binding.buttonSignIn.setOnClickListener{
+            if(email.text.isEmpty()){
+                email.error="Pls enter email!"
+                email.requestFocus()
+                return@setOnClickListener
+            }
+            if(password.text.isEmpty()){
+                password.error="Enter password"
+                password.requestFocus()
+                return@setOnClickListener
+            }
+            firebaseAuth.signInWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnSuccessListener {
+
+            }.addOnFailureListener{
+                Toast.makeText(requireContext(), "User couldn't Found", Toast.LENGTH_SHORT).show()
+            }
+
         }
 
         return binding.root
--- app/src/main/java/az/sharif/bippyteam/view/fragment/FragmentSignUp.kt
+++ app/src/main/java/az/sharif/bippyteam/view/fragment/FragmentSignUp.kt
@@ -4,17 +4,76 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import az.sharif.bippyteam.databinding.FragmentSignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class FragmentSignUp: Fragment() {
     private lateinit var binding :FragmentSignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        firebaseAuth = Firebase.auth
+    }
+
+
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSignUpBinding.inflate(layoutInflater)
+        var name = binding.inputName
+        var email = binding.inputEmail
+        var password = binding.inputPassword
+        var confirmPassword = binding.inputConfirmPassword
+
+        /////////// AUTHENTICATION ///////////
+
+        binding.buttonSignUp.setOnClickListener{
+
+            if(name.text.isEmpty()){
+                name.error = "Name cannot be empty!"
+                name.requestFocus()
+                return@setOnClickListener
+            }
+            if(email.text.isEmpty()){
+                email.requestFocus()
+                email.error = "Email cannot be empty"
+                return@setOnClickListener
+
+            }
+            if(password.text.isEmpty()|| password.text.length<5){
+                password.requestFocus()
+                password.error = "Password must contain at least 6 character"
+                return@setOnClickListener
+
+            }
+            if(confirmPassword.text.isEmpty() || confirmPassword.text.toString()!=password.text.toString()){
+                confirmPassword.requestFocus()
+                confirmPassword.error = "Did you already forget your password?"
+                return@setOnClickListener
+
+            }
+            firebaseAuth.createUserWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnSuccessListener {
+                Toast.makeText(requireContext(), "User added", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener{
+                Toast.makeText(requireContext(), "Error 404", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+        }
+
         return binding.root
     }
 }
