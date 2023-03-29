21fbc4ed24972ac5c71a4031e2403d2eda1d4d2c
Jafar Tarverdiyev <cefertarverdiyevv@gmail.com>
2023-03-29 12:27:15 +0400

    DebutV2

--- app/src/main/java/az/sharif/bippyteam/view/activity/MainActivity.kt
+++ app/src/main/java/az/sharif/bippyteam/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package az.sharif.bippyteam
+package az.sharif.bippyteam.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import az.sharif.bippyteam.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
