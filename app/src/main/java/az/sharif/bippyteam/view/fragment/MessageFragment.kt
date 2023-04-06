1dd8290c6156c1aae1dd20efb18ab00ecb25a69d
Qurban Yusufov <qurbanyusifov89@gmail.com>
2023-04-06 10:51:16 +0400

    Chat page design created

--- app/src/main/java/az/sharif/bippyteam/view/fragment/MessageFragment.kt
+++ app/src/main/java/az/sharif/bippyteam/view/fragment/MessageFragment.kt
@@ -1,6 +1,13 @@
 package az.sharif.bippyteam.view.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import az.sharif.bippyteam.R
 
-class MessageFragment: Fragment() {
+class MessageFragment: Fragment(R.layout.fragment_message) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
